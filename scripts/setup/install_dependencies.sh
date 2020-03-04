fc487d269680f0adccafa13f246f4e5d922dbab4
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2020-03-04 17:50:22 +0800

    Some more fixes again...
    Co-Authored-By: The Pins Team <teamalaspinsph@gmail.com>

--- scripts/setup/install_dependencies.sh
+++ scripts/setup/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Installing dependencies..."
-pip install --user -r requirements.txt
+python3 -m pip3 install --user Markdown Pygments pymdown-extensions mkdocs mkdocs-material mkdocs-redirects mkdocs-git-revision-date-localized-plugin
 echo "Success!"
