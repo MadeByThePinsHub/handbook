2f9f472ddc0b619fc001260e93f6dc219ead1caf
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2020-04-02 11:00:18 +0000

    Simplified builds script edited + Gitpod Config file update
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>

--- scripts/simplied_buildProcess.sh
+++ scripts/simplied_buildProcess.sh
@@ -4,12 +4,13 @@ bash ./setup/install_dependencies.sh
 echo ""
 sleep 2
 
-bash ./count-words.sh
+bash ./builds/count-words.sh
 echo ""
 sleep 2
 
-bash ./mkdocs_build.sh
+bash ./builds/mkdocs_build.sh
 echo ""
 sleep 2
 
 echo "Simplied build process script finished! If you catched some errors, please create a issue in our Issue Tracker."
+exit 1
