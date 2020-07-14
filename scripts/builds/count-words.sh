41b4370ec2c6764a45af95c4826f304aa6721574
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2020-07-14 23:40:08 +0800

    [scripts] Update the Word Counter script
    As usual, use Bash instead of sh due to compatibility mess.

--- scripts/builds/count-words.sh
+++ scripts/builds/count-words.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "#########################"
+ecbo "Word Counter"
 echo ""
 echo "The results of word counter as of $(date +%D) is:"
 echo "$(find handbook-src -type f | xargs wc -w)"
